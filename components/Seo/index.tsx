3c916908fc7d399e35d0892a4171cbddf54ed3a4
Shehab Emon <emonhasan20002000@gmail.com>
2024-04-04 20:26:00 +0100

    - fixed title for seo

--- components/Seo/index.tsx
+++ components/Seo/index.tsx
@@ -46,7 +46,7 @@ const Seo = ({ seo }: Props) => {
       <meta name="keywords" content={seo?.keywords || keywords} />
 
       {/* Canonical Link Tag */}
-      <link rel="canonical" href={path} />
+      <link rel="canonical" href={path || ""} />
 
       {/* Site Open Graph Tags */}
       <meta content="en_GB" property="og:locale" />
@@ -56,7 +56,7 @@ const Seo = ({ seo }: Props) => {
       <meta content={siteName} name="site_name" />
 
       {/* Content Open Graph Tags */}
-      <meta property="og:title" content={seo?.title || title || "" || ""} />
+      <meta property="og:title" content={seo?.title || siteName || ""} />
       <meta
         property="og:description"
         content={seo?.description || description}
@@ -65,10 +65,10 @@ const Seo = ({ seo }: Props) => {
         property="og:image"
         content={seo?.image?.asset?.url || image?.asset?.url}
       />
-      <meta property="og:url" content={path} />
+      <meta property="og:url" content={path || ""} />
 
       {/*<!-- Google / Search Engine Tags -->*/}
-      <meta property="name" content={seo?.title || title || ""} />
+      <meta property="name" content={seo?.title || siteName || ""} />
       <meta property="description" content={seo?.description || description} />
       <meta
         property="image"
@@ -76,7 +76,7 @@ const Seo = ({ seo }: Props) => {
       />
 
       {/*<!-- Twitter Meta Tags -->*/}
-      <meta name="twitter:title" content={seo?.title || title || ""} />
+      <meta name="twitter:title" content={seo?.title || siteName || ""} />
       <meta
         name="twitter:description"
         content={seo?.description || description}
